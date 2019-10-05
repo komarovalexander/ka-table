9df893c3fe4e8f3576d58bd2841a68d1e2125a16
Komarov Alexander <sanrkom@gmail.com>
2019-10-05 22:02:18 +0200

    Get rid from styleguidist

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React from 'react';
 
-import Table, { ITableOption } from './Components/Table/Table';
-import { SortDirection } from './Enums/SortDirection';
-import { DefaultOptions } from './Models/DefaultOptions';
-import { OptionChangedParam } from './Models/EventParams/OptionChangedParam';
-
-DefaultOptions.columnSortDirection = SortDirection.Ascend;
-const dataArray: any[] = [];
-
-for (let index = 0; index < 10; index++) {
-  dataArray.push({ column: index + '1', column2: index + '2', id: index });
-}
-
-const tableOption: ITableOption = {
-  columns: [
-    { field: 'id', title: 'Id', sortDirection: SortDirection.Descend },
-    { field: 'column', title: 'Column 1' },
-    { field: 'column2', title: 'Column 2' },
-  ],
-  rowKey: 'id',
-};
+import BasicDemo from './Demos/BasicDemo';
 
 const App: React.FC = () => {
-  const [option, changeOptions] = useState(tableOption);
-  const onOptionChanged = (newOption: OptionChangedParam) => {
-    changeOptions({...option, ...newOption.value });
-  };
   return (
-    <div className='App'>
-      <Table
-        {...option}
-        data={dataArray}
-        onOptionChanged={onOptionChanged}
-      />
-    </div>
+    <BasicDemo />
   );
 };
 
---
+++ src/Demos/BasicDemo.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import Table, { ITableOption } from '../Components/Table/Table';
+import { OptionChangedParam } from '../Models/EventParams/OptionChangedParam';
+
+const dataArray: any[] = [];
+
+for (let index = 0; index < 10; index++) {
+  dataArray.push({ column: index + '1', column2: index + '2', id: index });
+}
+
+const tableOption: ITableOption = {
+  columns: [
+    { field: 'id', title: 'Id' },
+    { field: 'column', title: 'Column 1' },
+    { field: 'column2', title: 'Column 2' },
+  ],
+  rowKey: 'id',
+};
+
+const BasicDemo: React.FC = () => {
+  const [option, changeOptions] = useState(tableOption);
+  const onOptionChanged = (newOption: OptionChangedParam) => {
+    changeOptions({...option, ...newOption.value });
+  };
+  return (
+    <Table
+      {...option}
+      data={dataArray}
+      onOptionChanged={onOptionChanged}
+    />
+  );
+};
+
+export default BasicDemo;
