fbfae86974d32cf305c24fb81d04251643163692
Komarov Alexander <sanrkom@gmail.com>
2019-11-20 21:21:30 +0100

    Update 2019-11-20T20:21:27.550Z

---
+++ demos/CommandColumnDemo/CommandColumnDemo.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+import { ITableOption, Table } from '../../lib';
+import { DataType } from '../../lib/enums';
+import { CellFuncPropsWithChildren, EventFunc, OptionChangedFunc } from '../../lib/types';
+
+const DELETE_EVENT = 'delete';
+
+const dataArray = Array(10).fill(undefined).map(
+  (_, index) => ({
+    column1: `column:1 row:${index}`,
+    column2: `column:2 row:${index}`,
+    column3: `column:3 row:${index}`,
+    column4: `column:4 row:${index}`,
+  }),
+);
+
+const AlertCell: React.FC<CellFuncPropsWithChildren> = ({
+   rowData,
+}) => {
+  return (
+    <div>
+      <button onClick={() => alert(`row data: ${JSON.stringify(rowData)}`)}>Show allert</button>
+    </div>
+  );
+};
+
+const DeleteRow: React.FC<CellFuncPropsWithChildren> = ({
+  rowData, onEvent,
+}) => {
+ return (
+   <div>
+     <button onClick={() => onEvent(DELETE_EVENT, { rowData })}>Delete</button>
+   </div>
+ );
+};
+
+const tableOption: ITableOption = {
+  columns: [
+    { key: 'command2', field: '', cell: (props) => <AlertCell {...props}/> },
+    { field: 'column1', title: 'Column 1', dataType: DataType.String },
+    { field: 'column2', title: 'Column 2', dataType: DataType.String },
+    { field: 'column3', title: 'Column 3', dataType: DataType.String },
+    { field: 'column4', title: 'Column 4', dataType: DataType.String },
+    { key: 'command1', field: '', cell: (props) => <DeleteRow {...props} /> },
+  ],
+  rowKey: 'column1',
+};
+
+const CommandColumnDemo: React.FC = () => {
+  const [option, changeOptions] = useState(tableOption);
+  const onOptionChanged: OptionChangedFunc = (value) => {
+    changeOptions({...option, ...value });
+  };
+
+  const [data, changeData] = useState(dataArray);
+  const onDataChanged: OptionChangedFunc = (newValue) => {
+    changeData(newValue);
+  };
+
+  const onEvent: EventFunc = (event, eventData) => {
+    if (event === DELETE_EVENT) {
+      const newValue = data.filter((d: any) => d[tableOption.rowKey] !== eventData.rowData[tableOption.rowKey]);
+      changeData(newValue);
+    }
+  };
+  return (
+    <Table
+      {...option}
+      data={data}
+      onOptionChanged={onOptionChanged}
+      onDataChanged={onDataChanged}
+      onEvent={onEvent}
+    />
+  );
+};
+
+export default CommandColumnDemo;
---
+++ demos/SelectionDemo/SelectionDemo.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+import { ITableOption, Table } from '../../lib';
+import { DataType, Events, SortDirection, SortingMode } from '../../lib/enums';
+import { EditorFuncPropsWithChildren, OptionChangedFunc } from '../../lib/types';
+
+const dataArray: any[] = [
+  { id: 1, name: 'Mike Wazowski', score: 80, passed: true },
+  { id: 2, name: 'Billi Bob', score: 55, passed: false },
+  { id: 3, name: 'Tom Williams', score: 45, passed: false },
+  { id: 4, name: 'Kurt Cobain', score: 75, passed: true },
+  { id: 5, name: 'Marshall Bruce', score: 77, passed: true },
+  { id: 6, name: 'Sunny Fox', score: 33, passed: false },
+];
+
+const SelectionCell: React.FC<EditorFuncPropsWithChildren> = ({
+  rowData, rowKey, onEvent, isSelectedRow,
+}) => {
+  return (
+    <input
+      type='checkbox'
+      checked={isSelectedRow}
+      onChange={(event) => {
+        const rowKeyValue = rowData[rowKey];
+        if (event.currentTarget.checked) {
+          onEvent(Events.RowSelected, { rowKeyValue });
+        } else {
+          onEvent(Events.RowDeselected, { rowKeyValue });
+        }
+      }}
+    />
+  );
+};
+
+const tableOption: ITableOption = {
+  columns: [
+    { key: 'commandColumn:selection', field: '', editor: SelectionCell, isEditable: true },
+    { field: 'name', title: 'Name', dataType: DataType.String, width: '33%', sortDirection: SortDirection.Descend },
+    { field: 'score', title: 'Score', width: '10%', dataType: DataType.Number },
+    { field: 'passed', title: 'Passed', dataType: DataType.Boolean },
+  ],
+  rowKey: 'id',
+  selectedRows: [3, 5],
+  sortingMode: SortingMode.Single,
+};
+
+const SelectionDemo: React.FC = () => {
+  const [option, changeOptions] = useState(tableOption);
+  const onOptionChanged: OptionChangedFunc = (value) => {
+    changeOptions({...option, ...value });
+  };
+  return (
+    <Table
+      {...option}
+      data={dataArray}
+      onOptionChanged={onOptionChanged}
+    />
+  );
+};
+
+export default SelectionDemo;
