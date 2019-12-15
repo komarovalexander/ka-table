647cac7787daa9797f5aec11b27b6e7022847b21
Komarov Alexander <sanrkom@gmail.com>
2019-12-15 23:17:35 +0100

    Update 2019-12-15T22:17:32.086Z

--- precache-manifest.0cf640c09dccd73e623315d8d1c195c6.js
+++ precache-manifest.0cf640c09dccd73e623315d8d1c195c6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8f1ca4712d6c184e35bdfc848e872d5b",
+    "revision": "c4fb32c103bffc62e8d3d6f5fbc69843",
     "url": "/ka-table/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ka-table/static/css/2.ab3f4a0d.chunk.css"
   },
   {
-    "revision": "af6d6bb3baef1b11816f",
-    "url": "/ka-table/static/css/main.c50c20e9.chunk.css"
+    "revision": "c2471db546c452d9fdd3",
+    "url": "/ka-table/static/css/main.f188e70a.chunk.css"
   },
   {
     "revision": "ede2a08ca24b1bd8dc32",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ka-table/static/js/2.2b27bc7b.chunk.js.LICENSE"
   },
   {
-    "revision": "af6d6bb3baef1b11816f",
-    "url": "/ka-table/static/js/main.69518e72.chunk.js"
+    "revision": "c2471db546c452d9fdd3",
+    "url": "/ka-table/static/js/main.f8e05844.chunk.js"
   },
   {
     "revision": "9d9c8dd1b5cfa2ec1df4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ka-table/precache-manifest.0dcefd2528bc669037b0b7bb036afa0f.js"
+  "/ka-table/precache-manifest.0cf640c09dccd73e623315d8d1c195c6.js"
 );
 
 self.addEventListener('message', (event) => {
