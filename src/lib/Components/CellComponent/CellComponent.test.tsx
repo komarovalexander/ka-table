7c6895cb5615ef38a65915a0b9ba01199eaa82bc
Komarov Alexander <sanrkom@gmail.com>
2019-12-27 20:33:54 +0100

    Add test for RowDataChanged

--- src/lib/Components/CellComponent/CellComponent.test.tsx
+++ src/lib/Components/CellComponent/CellComponent.test.tsx
@@ -1,9 +1,14 @@
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import sinon from 'sinon';
 
-import { DataType } from '../../enums';
-import CellComponent, { ICellComponentProps } from './CellComponent';
+import { DataType, Events } from '../../enums';
+import CellEditor from '../CellEditor/CellEditor';
+import CellComponent from './CellComponent';
 
+Enzyme.configure({ adapter: new Adapter() });
 const props: any = {
   column: {
     dataType: DataType.String,
@@ -23,4 +28,18 @@ it('renders without crashing', () => {
   const element = document.createElement('tr');
   ReactDOM.render(<CellComponent {...props} />, element);
   ReactDOM.unmountComponentAtNode(element);
+});
+
+it('call RowDataChanged when onValueChange', () => {
+  const dispatch = sinon.spy();
+  const newValue = {a: 1};
+  const wrapper = mount(
+    <CellComponent {...props} isEditableCell='true' dispatch={dispatch} />,
+    {
+      attachTo: document.createElement('tr'),
+    },
+  );
+  wrapper.find(CellEditor).last().prop('onValueChange')(newValue);
+  expect(dispatch.calledOnce).toBeTruthy();
+  expect(dispatch.args).toMatchSnapshot();
 });
