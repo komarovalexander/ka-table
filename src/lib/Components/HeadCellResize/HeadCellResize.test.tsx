8a68f793157417298b0583095108332d19ef1710
Komarov Alexander <sanrkom@gmail.com>
2020-07-09 00:17:53 +0200

    Add test

--- src/lib/Components/HeadCellResize/HeadCellResize.test.tsx
+++ src/lib/Components/HeadCellResize/HeadCellResize.test.tsx
@@ -1,12 +1,17 @@
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import simulant from 'simulant';
 
 import { Column } from '../../Models/Column';
 import HeadCellResize from './HeadCellResize';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 const props = {
   column: new Column(),
-  dispatch: () => {},
+  dispatch: jest.fn(),
   currentWidth: 100,
 };
 
@@ -14,4 +19,11 @@ it('renders without crashing', () => {
   const element = document.createElement('div');
   ReactDOM.render(<HeadCellResize {...props} />, element);
   ReactDOM.unmountComponentAtNode(element);
+});
+
+it('should handle onMouseDown correctly', () => {
+  const wrapper = mount(<HeadCellResize {...props} />, { attachTo: document.body });
+  wrapper.simulate('mousedown');
+  simulant.fire(document.body, 'mouseup');
+  expect(props.dispatch).toBeCalledTimes(2)
 });
