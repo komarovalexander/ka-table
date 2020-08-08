f9031f560de35a9054c834f0d1b0d60ad190281f
Komarov Alexander <sanrkom@gmail.com>
2020-08-08 22:52:40 +0200

    Fix column width

--- src/Demos/BootstrapDemo/BootstrapDemo.tsx
+++ src/Demos/BootstrapDemo/BootstrapDemo.tsx
@@ -20,10 +20,10 @@ const dataArray = Array(180).fill(undefined).map(
 
 const tablePropsInit: ITableProps = {
   columns: [
-    { key: 'column1', title: 'Column 1', dataType: DataType.Boolean },
-    { key: 'column2', title: 'Column 2', dataType: DataType.String },
-    { key: 'column3', title: 'Column 3', dataType: DataType.Number },
-    { key: 'column4', title: 'Column 4', dataType: DataType.Date },
+    { key: 'column1', title: 'Column 1', dataType: DataType.Boolean, style: {minWidth: 130}   },
+    { key: 'column2', title: 'Column 2', dataType: DataType.String, style: {width: 240} },
+    { key: 'column3', title: 'Column 3', dataType: DataType.Number, filterRowOperator: '>', style: {width: 230}  },
+    { key: 'column4', title: 'Column 4', dataType: DataType.Date, filterRowOperator: '<', style: {minWidth: 100} },
   ],
   format: ({ column, value }) => {
     if (column.dataType === DataType.Date){
