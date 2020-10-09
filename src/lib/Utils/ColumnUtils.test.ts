f98ff2cf73159c03d0a4f57efd4a555c2b1222c9
Komarov Alexander <sanrkom@gmail.com>
2020-10-09 21:00:54 +0200

    Add more tests

--- src/lib/Utils/ColumnUtils.test.ts
+++ src/lib/Utils/ColumnUtils.test.ts
@@ -1,14 +1,19 @@
 
-import { getField, getFieldParts, getLastFieldParents } from './ColumnUtils';
+import defaultOptions from '../defaultOptions';
+import { getField, getFieldParts, getLastField, getLastFieldParents } from './ColumnUtils';
 
 describe('ColumnUtils', () => {
   it('getField from field', () => {
-    const field = getField({ key: '2', field: '3' });
-    expect(field).toEqual('3');
+    const field = getField({ key: '2.2', field: '2.3' });
+    expect(field).toEqual('2.3');
   });
   it('getField from key', () => {
-    const field = getField({ key: '2' });
-    expect(field).toEqual('2');
+    const field = getField({ key: '2.2' });
+    expect(field).toEqual('2.2');
+  });
+  it('getLastField', () => {
+    const fields = getLastField('prop1.prop2.prop3');
+    expect(fields).toEqual('prop3');
   });
   it('getLastFieldParents', () => {
     const fields = getLastFieldParents('prop1.prop2.prop3');
@@ -17,5 +22,38 @@ describe('ColumnUtils', () => {
   it('getFieldParts', () => {
     const fields = getFieldParts('prop1.prop2.prop3');
     expect(fields).toEqual(['prop1', 'prop2', 'prop3']);
+  });
+  describe('ColumnUtils without delimiter', () => {
+    beforeEach(() => {
+      defaultOptions.fieldDelimiter = '';
+    });
+    afterEach(() => {
+      defaultOptions.fieldDelimiter = '.';
+    });
+
+    it('getField from field', () => {
+      const field = getField({ key: '2.2', field: '2.3' });
+      expect(field).toEqual('2.3');
+    });
+
+    it('getField from key', () => {
+      const field = getField({ key: '2.2' });
+      expect(field).toEqual('2.2');
+    });
+
+    it('getLastField', () => {
+      const fields = getLastField('prop1.prop2.prop3');
+      expect(fields).toEqual('prop1.prop2.prop3');
+    });
+
+    it('getLastFieldParents', () => {
+      const fields = getLastFieldParents('prop1.prop2.prop3');
+      expect(fields).toEqual([]);
+    });
+
+    it('getFieldParts', () => {
+      const fields = getFieldParts('prop1.prop2.prop3');
+      expect(fields).toEqual(['prop1.prop2.prop3']);
+    });
   });
 });
