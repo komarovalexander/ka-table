10aab0abdffcb67d59a1b0b3a78096461af9372b
Komarov Alexander <sanrkom@gmail.com>
2020-10-09 20:42:26 +0200

    Add tests

--- src/lib/Utils/ColumnUtils.test.ts
+++ src/lib/Utils/ColumnUtils.test.ts
@@ -1,5 +1,5 @@
 
-import { getField } from './ColumnUtils';
+import { getField, getFieldParts, getLastFieldParents } from './ColumnUtils';
 
 describe('ColumnUtils', () => {
   it('getField from field', () => {
@@ -9,5 +9,13 @@ describe('ColumnUtils', () => {
   it('getField from key', () => {
     const field = getField({ key: '2' });
     expect(field).toEqual('2');
+  });
+  it('getLastFieldParents', () => {
+    const fields = getLastFieldParents('prop1.prop2.prop3');
+    expect(fields).toEqual(['prop1', 'prop2']);
+  });
+  it('getFieldParts', () => {
+    const fields = getFieldParts('prop1.prop2.prop3');
+    expect(fields).toEqual(['prop1', 'prop2', 'prop3']);
   });
 });
