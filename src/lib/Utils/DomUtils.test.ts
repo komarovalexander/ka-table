b880bd82cd893f9dc534df813235ee2fdd24f52c
Komarov Alexander <sanrkom@gmail.com>
2020-12-27 20:20:50 +0100

    try to fix coveralls

--- src/lib/Utils/DomUtils.test.ts
+++ src/lib/Utils/DomUtils.test.ts
@@ -54,6 +54,22 @@ describe('DomUtils', () => {
       expect(elementRef.current.childNodes[0].style.top).toBe(0);
       expect(elementRef.current.childNodes[1].style.top).toBe(0);
     });
+    it('child childNodes is undefined', () => {
+      const elementRef = {
+        current: {
+          parentElement: {
+            previousSibling: {
+              getBoundingClientRect: () => 10
+            }
+          },
+          style: {
+            top: 0
+          },
+        }
+      };
+      updateChildrenTop(elementRef);
+      expect(elementRef.current.style.top).toBe(0);
+    });
     it('height is already set', () => {
       const optionSet = jest.fn();
       const elementRef = {
