f46f90d051f4e908b39d66e8c6f71e1cbe6f58dd
Komarov Alexander <sanrkom@gmail.com>
2021-01-30 19:08:34 +0100

    Add more tests

--- src/lib/Components/PagingSize/PagingSize.test.tsx
+++ src/lib/Components/PagingSize/PagingSize.test.tsx
@@ -3,6 +3,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { ActionType } from '../../enums';
 import { IPagingSizeProps } from '../../props';
 import PagingSize from './PagingSize';
 
@@ -10,7 +11,7 @@ Enzyme.configure({ adapter: new Adapter() });
 
 const props: IPagingSizeProps = {
   childComponents: {},
-  value: 1
+  value: 5
 };
 
 it('renders without crashing', () => {
@@ -21,4 +22,15 @@ it('renders without crashing', () => {
 it('should render with custom content', () => {
   const wrapper = mount(<PagingSize {...props} childComponents={{ pagingSize: { content: () => (<>Custom Paging Size</>) }}} />);
   expect(wrapper.find('.ka-paging-size').text()).toEqual('Custom Paging Size');
+});
+
+it('onClick should dispath UpdatePageSize on click', () => {
+  const dispatch = jest.fn();
+  const wrapper = mount(<PagingSize {...props} dispatch={dispatch} />);
+  wrapper.find('.ka-paging-size').first().simulate('click');
+  expect(dispatch).toBeCalledTimes(1);
+  expect(dispatch).toBeCalledWith({
+    pageSize: 5,
+    type: ActionType.UpdatePageSize,
+  });
 });
