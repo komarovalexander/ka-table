fe21628190d9cfef1b9b9dd6d4b0840ed855593c
Kossskom <ya.kom345@yandex.ru>
2022-10-25 15:17:14 +0300

    AlertCellDemo NewFix

--- src/Demos/AlertCellDemo/AlertCellDemo.tsx
+++ src/Demos/AlertCellDemo/AlertCellDemo.tsx
@@ -28,19 +28,6 @@ const AlertCell: React.FC<ICellTextProps> = ({
   );
 };
 
-const tablePropsInit: ITableProps = {
-  columns: [
-    { key: 'command1', style: { textAlign: 'center' }, width: 70 },
-    { key: 'column1-1', field: 'column1', title: 'Column 1', dataType: DataType.String },
-    { key: 'column1-2', field: 'column1', title: 'Column 1', dataType: DataType.String },
-    { key: 'column2', title: 'Column 2', dataType: DataType.String },
-    { key: 'column3', title: 'Column 3', dataType: DataType.String },
-    { key: 'column4', title: 'Column 4', dataType: DataType.String },
-  ],
-  data: dataArray,
-  rowKeyField: 'id',
-};
-
 const AlertCellDemo: React.FC = () => {
   return (
     <Table
