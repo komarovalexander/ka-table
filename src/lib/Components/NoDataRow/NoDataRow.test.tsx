ad42b42b895262d03fa9c2c2053b3f16b17477cd
Alexander Komarov <k.alex@qvedo.com>
2023-02-14 22:58:53 +0100

    Add noDataCell childComponent

--- src/lib/Components/NoDataRow/NoDataRow.test.tsx
+++ src/lib/Components/NoDataRow/NoDataRow.test.tsx
@@ -1,7 +1,12 @@
+import Enzyme, { mount } from 'enzyme';
+
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { INoDataRowProps } from '../../props';
 import NoDataRow from './NoDataRow';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 const props: INoDataRowProps = {
     childComponents: {},
@@ -13,4 +18,18 @@ it('renders without crashing', () => {
     const div = document.createElement('tbody');
     ReactDOM.render(<NoDataRow {...props} />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it('custom colspan for cell', () => {
+    const wrapper = mount(<NoDataRow {...props} childComponents={{
+        noDataCell: {
+            elementAttributes:() => ({
+                colSpan: 100
+            })
+        }
+    }}/>, {
+        attachTo: document.createElement('tbody')
+    });
+    expect(wrapper.find('.ka-no-data-cell').exists()).toBeTruthy();
+    expect(wrapper.find('.ka-no-data-cell').prop('colSpan')).toBe(100);
 });
