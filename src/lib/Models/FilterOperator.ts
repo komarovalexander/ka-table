c4d7481f4c271a0025b404b103d55282df47aebd
Alexander Komarov <k.alex@qvedo.com>
2023-05-18 20:35:34 +0600

    rowData: any[]

--- src/lib/Models/FilterOperator.ts
+++ src/lib/Models/FilterOperator.ts
@@ -1,7 +1,7 @@
 import { DataType } from '../enums';
 
 export class FilterOperator {
-  public compare!: (fieldValue: any, conditionValue: any, rowData?: any) => boolean;
+  public compare!: (fieldValue: any, conditionValue: any, rowData?: any[]) => boolean;
   public defaultForTypes?: DataType[];
   public name!: string;
 }
