e60b5808bbb36a8be0666b9e4e95895236d6bd6d
Alexander Komarov <k.alex@qvedo.com>
2024-01-16 17:39:02 +0300

    Add kaPagingUtils to the utils import

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,10 +3,12 @@ import * as kaColumnUtils from './Utils/ColumnUtils';
 import * as kaDateUtils from './Utils/DateUtils';
 import * as kaPropsUtils from './Utils/PropsUtils';
 import * as kaTypeUtils from './Utils/TypeUtils';
+import * as kaPagingUtils from './Utils/PagingUtils';
 
 export {
     kaColumnUtils,
     kaDateUtils,
     kaPropsUtils,
     kaTypeUtils,
+    kaPagingUtils
 };
